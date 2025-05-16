563e6d09073457d8b1d73faa613820c8b195db47
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-05-16 11:33:19 +0530

    sequelize added

--- models/product.js
+++ models/product.js
@@ -1,48 +1,89 @@
-const db = require('../util/database');
-// const fs = require('fs');
-// const path = require('path');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   'data',
-//   'products.json'
-// );
-
-// const getProductsFromFile = cb => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       cb([]);
-//     } else {
-//       cb(JSON.parse(fileContent));
-//     }
-//   });
-// };
-
-module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
+module.exports = Product;
 
-  save() {
-    return db.execute('INSERT INTO products (title,price,description,imgUrl) VALUES (?,?,?,?)',[this.title,this.price,this.description,this.imageUrl]);}
-    // this.id = Math.random().toString();
-    // getProductsFromFile(products => {
-    //   products.push(this);
-    //   fs.writeFile(p, JSON.stringify(products), err => {
-    //     console.log(err);
-    //   });
-    // });
+
+
+
+
+
+
+
+
+
+
+
+
+// const db = require('../util/database');
+// // const fs = require('fs');
+// // const path = require('path');
+
+// // const p = path.join(
+// //   path.dirname(process.mainModule.filename),
+// //   'data',
+// //   'products.json'
+// // );
+
+// // const getProductsFromFile = cb => {
+// //   fs.readFile(p, (err, fileContent) => {
+// //     if (err) {
+// //       cb([]);
+// //     } else {
+// //       cb(JSON.parse(fileContent));
+// //     }
+// //   });
+// // };
+
+// module.exports = class Product {
+//   constructor(title, imageUrl, description, price) {
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   save() {
+//     return db.execute('INSERT INTO products (title,price,description,imgUrl) VALUES (?,?,?,?)',[this.title,this.price,this.description,this.imageUrl]);}
+//     // this.id = Math.random().toString();
+//     // getProductsFromFile(products => {
+//     //   products.push(this);
+//     //   fs.writeFile(p, JSON.stringify(products), err => {
+//     //     console.log(err);
+//     //   });
+//     // });
   
 
-  static fetchAll(cb) {
-   return db.execute('SELCT * FROM products');
-    // getProductsFromFile(cb);
-  }
-  static findById(id, cb) {
-   return db.execute('SELECT * FROM products WHERE id = ?',[id]);
-  }
+//   static fetchAll(cb) {
+//    return db.execute('SELCT * FROM products');
+//     // getProductsFromFile(cb);
+//   }
+//   static findById(id, cb) {
+//    return db.execute('SELECT * FROM products WHERE id = ?',[id]);
+//   }
     
-};
+// };
--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-  host: 'localhost',
-  user:'rootnode',
-    database: 'node-complete',
-    password: 'rootnode'
+// const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'rootnode', 'rootnode', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
-module.exports = pool.promise();
+module.exports = sequelize;
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user:'rootnode',
+//     database: 'node-complete',
+//     password: 'rootnode'
+// });
+// module.exports = pool.promise();
