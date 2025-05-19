a42ef0c04751c6b0773cef14308b2d8e26308a6d
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-05-19 21:37:38 +0530

    some chnages

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 // const mysql = require('mysql2');
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'rootnode', 'rootnode', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-module.exports = sequelize;
+// const sequelize = new Sequelize('node-complete', 'rootnode', 'rootnode', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+// module.exports = sequelize;
 // const pool = mysql.createPool({
 //   host: 'localhost',
 //   user:'rootnode',
@@ -13,3 +13,9 @@ module.exports = sequelize;
 //     password: 'rootnode'
 // });
 // module.exports = pool.promise();
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const MongoCpnnect = (callback) => {
+  MongoClient.connect('mongodb+srv://anuragtiwaris1172000:@nodejs.sy8eb9i.mongodb.net/')
+}
